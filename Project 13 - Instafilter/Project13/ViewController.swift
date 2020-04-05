3cbb32647a63ccf284d9eba5236924239090a48d
Mike <cyberenot@gmail.com>
2020-04-05 20:00:26 +0300

    Project13

---
+++ Project 13 - Instafilter/Project13/ViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Mike on 2019-07-25.
+//  Copyright © 2019 Mike. All rights reserved.
+//
+
+import CoreImage
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var radius: UISlider!
+    @IBOutlet var filterButton: UIButton!
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Instafilter" // "YACIFP" - Yet Another Core Image Filter Program
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        imageView.alpha = 0
+        dismiss(animated: true, completion: pictureFadeInAnimation)
+        currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+        if currentImage != nil {
+            let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+            ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
+            if let popoverController = ac.popoverPresentationController {
+                popoverController.sourceView = sender
+                popoverController.sourceRect = sender.bounds
+            }
+            
+            present(ac, animated: true)
+        } else {
+             showMessage(title: "Error!", message: "There's no image.")
+        }
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else { return }
+        guard let actionTitle = action.title else { return }
+        
+        currentFilter = CIFilter(name: actionTitle)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        filterButton.setTitle(actionTitle, for: .normal)
+        
+        applyProcessing()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else {
+            showMessage(title: "Error!", message: "There's no image.")
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    @IBAction func radiusChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+        
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return } // safe unwrapping
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let processedImage = UIImage(cgImage: cgImage)
+            imageView.image = processedImage
+        }
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            showMessage(title: "Save error", message: error.localizedDescription)
+        } else {
+            showMessage(title: "Saved!", message: "Your altered image has been saved to your photos.")
+        }
+    }
+    
+    func showMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func pictureFadeInAnimation() {
+        UIView.animate(withDuration: 2, animations: {
+            self.imageView.alpha = 1
+        })
+    }
+}
+
---
+++ Project 13 - Instafilter/README.md
@@ -0,0 +1,1 @@
+Photo manupulation app (Core Image, UISlider, writing to the photo library)
