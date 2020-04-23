22befc74b5af1a1971e9379337345f8b96cd262d
Mike <cyberenot@gmail.com>
2020-04-23 23:01:59 +0300

    Project27 - task2

--- Project 27 - Core Graphics/Project27/ViewController.swift
+++ Project 27 - Core Graphics/Project27/ViewController.swift
@@ -1,14 +1,14 @@
  //
-//  ViewController.swift
-//  Project27
-//
-//  Created by Mike on 2020-04-22.
-//  Copyright © 2020 Mike. All rights reserved.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
+ //  ViewController.swift
+ //  Project27
+ //
+ //  Created by Mike on 2020-04-22.
+ //  Copyright © 2020 Mike. All rights reserved.
+ //
+ 
+ import UIKit
+ 
+ class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     var currentDrawType = 0
     
@@ -19,11 +19,11 @@ class ViewController: UIViewController {
         
         drawRectangle()
     }
-
+    
     @IBAction func redrawTapped(_ sender: Any) {
         currentDrawType += 1
         
-        if currentDrawType > 6 {
+        if currentDrawType > 7 {
             currentDrawType = 0
         }
         
@@ -49,11 +49,14 @@ class ViewController: UIViewController {
         case 6:
             drawEmoji()
             
+        case 7:
+            drawTwin()
+            
         default:
             break
         }
     }
- 
+    
     func drawRectangle() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
         
@@ -128,9 +131,9 @@ class ViewController: UIViewController {
     }
     
     func drawLines() {
-          let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-          
-          let image = renderer.image { ctx in
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
             ctx.cgContext.translateBy(x: 256, y: 256)
             
             var first = true
@@ -151,10 +154,10 @@ class ViewController: UIViewController {
             
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.strokePath()
-          }
-          
-          imageView.image = image
-      }
+        }
+        
+        imageView.image = image
+    }
     
     func drawImagesAndText() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
@@ -180,25 +183,65 @@ class ViewController: UIViewController {
         imageView.image = image
     }
     
-    //1
+    // 1
     func drawEmoji() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-           
-           let image = renderer.image { ctx in
-               let paragraphStyle = NSMutableParagraphStyle()
-               paragraphStyle.alignment = .center
-               
-               let attrs: [NSAttributedString.Key: Any] = [
-                   .font: UIFont.systemFont(ofSize: 60),
-                   .paragraphStyle: paragraphStyle
-               ]
-               
+        
+        let image = renderer.image { ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 60),
+                .paragraphStyle: paragraphStyle
+            ]
+            
             let emoji = "⭐"
-               
+            
             let attributedString = NSAttributedString(string: emoji, attributes: attrs)
             attributedString.draw(with: CGRect(x: 32, y: 150, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
-           }
-           
-           imageView.image = image
+        }
+        
+        imageView.image = image
+    }
+    
+    // 2
+    func drawTwin() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            
+            // T
+            ctx.cgContext.move(to: CGPoint(x: 125, y: 200))
+            ctx.cgContext.addLine(to: CGPoint(x: 175, y: 200))
+            ctx.cgContext.move(to: CGPoint(x: 150, y: 200))
+            ctx.cgContext.addLine(to: CGPoint(x: 150, y: 260))
+            
+            // W
+            ctx.cgContext.move(to: CGPoint(x: 200, y: 200))
+            ctx.cgContext.addLine(to: CGPoint(x: 225, y: 260))
+            ctx.cgContext.move(to: CGPoint(x: 225, y: 260))
+            ctx.cgContext.addLine(to: CGPoint(x: 250, y: 200))
+            
+            ctx.cgContext.move(to: CGPoint(x: 225, y: 200))
+            ctx.cgContext.addLine(to: CGPoint(x: 250, y: 260))
+            ctx.cgContext.move(to: CGPoint(x: 250, y: 260))
+            ctx.cgContext.addLine(to: CGPoint(x: 275, y: 200))
+            
+            // I
+            ctx.cgContext.move(to: CGPoint(x: 300, y: 200))
+            ctx.cgContext.addLine(to: CGPoint(x: 300, y: 260))
+            
+            // N
+            ctx.cgContext.move(to: CGPoint(x: 325, y: 260))
+            ctx.cgContext.addLine(to: CGPoint(x: 325, y: 200))
+            ctx.cgContext.addLine(to: CGPoint(x: 370, y: 260))
+            ctx.cgContext.addLine(to: CGPoint(x: 370, y: 200))
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = image
     }
  }
