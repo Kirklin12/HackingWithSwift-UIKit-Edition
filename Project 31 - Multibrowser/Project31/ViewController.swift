8926eaac7d5c6946ffef5cb692fc1e6495ee38e1
Mike <cyberenot@gmail.com>
2020-05-07 19:46:29 +0300

    Project31 - iPad multitasking

--- Project 31 - Multibrowser/Project31/ViewController.swift
+++ Project 31 - Multibrowser/Project31/ViewController.swift
@@ -29,6 +29,12 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
         title = "Multibrowser"
     }
     
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if webView == activeWebView {
+            updateUI(for: webView)
+        }
+    }
+    
     @objc func addWebView() {
         let webView = WKWebView()
         webView.navigationDelegate = self
@@ -81,6 +87,8 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
         
         activeWebView = webView
         webView.layer.borderWidth = 3
+        
+        updateUI(for: webView)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -102,5 +110,18 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
     
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.horizontalSizeClass == .compact {
+            stackView.axis = .vertical
+        } else {
+            stackView.axis = .horizontal
+        }
+    }
+    
+    func updateUI(for webView: WKWebView) {
+        title = webView.title
+        addressBar.text = webView.url?.absoluteString ?? ""
     }
 }
