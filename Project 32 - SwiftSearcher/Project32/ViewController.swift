9a088c9637726788b9d742937726c6e43dbaddf3
Mike <cyberenot@gmail.com>
2020-05-08 00:28:42 +0300

    Project32 - Automatically resizing UITableViewCells with Dynamic Type and NSAttributedString

---
+++ Project 32 - SwiftSearcher/Project32/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Project32
+//
+//  Created by Mike on 2020-05-07.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var projects = [[String]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
+        projects.append(["Project 2: Guess the Flag", "Asset catalogs, UIButton, CALayer, UIColor, UIAlertController"])
+        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, URL"])
+        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, URLRequest, UIToolbar, UIProgressView, key-value observing"])
+        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
+        projects.append(["Project 6: Auto Layout", "NSLayoutConstraint, Visual Format Language, layout anchors"])
+        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
+        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count(), index(of:), joined(), property observers, range operators"])
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let project = projects[indexPath.row]
+        cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
+        
+        return cell
+    }
+    
+    func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline), NSAttributedString.Key.foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+        
+        let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttributes)
+        let subtitleString = NSAttributedString(string: subtitle, attributes: subtitleAttributes)
+        
+        titleString.append(subtitleString)
+        
+        return titleString
+    }
+}
+
---
+++ Project 32 - SwiftSearcher/README.md
@@ -0,0 +1,1 @@
+Core Spotlight, SFSafariViewController, NSAttributedString, Dynamic Type, automatic UITableViewCell sizing
