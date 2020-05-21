64cc21d137ebca18b23b09be039598ad801f758f
Mike <cyberenot@gmail.com>
2020-05-21 17:39:43 +0300

    Project36 - scoreLabel position fix

--- Project 36 - Crashy Plane/Project36/GameScene.swift
+++ Project 36 - Crashy Plane/Project36/GameScene.swift
@@ -158,7 +158,7 @@ class GameScene: SKScene {
         scoreLabel = SKLabelNode(fontNamed: "Optima-ExtraBlack")
         scoreLabel.fontSize = 24
         
-        scoreLabel.position = CGPoint(x: frame.maxX - 80, y: frame.maxY - 60)
+        scoreLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 60)
         scoreLabel.text = "SCORE: 0"
         scoreLabel.fontColor = UIColor.black
         
