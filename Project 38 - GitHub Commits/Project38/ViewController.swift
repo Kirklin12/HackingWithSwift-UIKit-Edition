4dceef2c092012189787823bf8e886523e5cff85
Mike <cyberenot@gmail.com>
2020-05-29 18:17:00 +0300

    Project38 - Adding Core Data to our project: NSPersistentContainer

--- Project 38 - GitHub Commits/Project38/ViewController.swift
+++ Project 38 - GitHub Commits/Project38/ViewController.swift
@@ -6,15 +6,32 @@
 //  Copyright © 2020 Mike. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var container: NSPersistentContainer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        container = NSPersistentContainer(name: "Project38")
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print("Unresolved error \(error)")
+            }
+        }
     }
-
-
+    
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occured while saving: \(error)")
+            }
+        }
+    }
 }
 
