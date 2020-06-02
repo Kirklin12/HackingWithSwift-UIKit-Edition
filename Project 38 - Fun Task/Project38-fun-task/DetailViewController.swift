fdbd6de1d1e970c399dc9d0abe2b542554d2aad8
Mike <cyberenot@gmail.com>
2020-06-02 22:09:31 +0300

    Project38 - Fun task

---
+++ Project 38 - Fun Task/Project38-fun-task/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project38-fun-task
+//
+//  Created by Mike on 2020-06-02.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
