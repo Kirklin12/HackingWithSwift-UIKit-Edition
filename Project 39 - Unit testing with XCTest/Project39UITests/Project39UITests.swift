75ca688616c82a97d96d532a9fbfca6af7606bcc
Mike <cyberenot@gmail.com>
2020-06-09 18:36:15 +0300

    Project39 - User interface testing with XCTest

--- Project 39 - Unit testing with XCTest/Project39UITests/Project39UITests.swift
+++ Project 39 - Unit testing with XCTest/Project39UITests/Project39UITests.swift
@@ -7,13 +7,16 @@
 //
 
 import XCTest
+@testable import Project39
 
 class Project39UITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        app.activate()
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
@@ -23,15 +26,6 @@ class Project39UITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
@@ -39,5 +33,23 @@ class Project39UITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    func testInitialStateIsCorrect() {
+        let table = XCUIApplication().tables
+        XCTAssertEqual(table.cells.count, 7, "There should be 7 rows initially")
+    }
+    
+    func testUserFilteringByString() {
+        let app = XCUIApplication()
+        app.buttons["Search"].tap()
+        
+        let filterAlert = app.alerts
+        let textField = filterAlert.textFields.element
+        textField.typeText("test")
+        
+        filterAlert.buttons["Filter"].tap()
+        
+        XCTAssertEqual(app.tables.cells.count, 56, "There should be 56 words matching 'test'")
     }
 }
